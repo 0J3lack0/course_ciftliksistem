bf95e3007e5dc43bce460accf75a327ad41ed20a
0J3lack0 <g.caglar.c@gmail.com>
2022-11-24 15:53:10 +0300

    Döngü uygulama örneği

---
+++ js/loopDemo.js
@@ -0,0 +1,14 @@
+//Loto sayıları üreten fonksiyon (1-49 arasında) ile birden fazla kolon için 6'lı loto sayıları üretmek
+//Bunun için iç içe döngüleri kullanabiliriz. 
+function genLotoNumb(maxValue = 49){
+  return Math.ceil(Math.random()*maxValue)
+}
+
+for(var i = 1; i<=8;i++){
+  console.log(i + '.Columns');
+  for(var j = 1; j<=6; j++){
+    console.log(genLotoNumb());
+  }
+}
+
+
